098e77b163afac4d6876650f5d1d5a3392f3174c
Robin Madsen <robin@robserob.dk>
2012-03-29 00:24:02 +0200

    Better input method and comparesion

--- index.php
+++ index.php
@@ -99,13 +99,20 @@ function decompress($str, $xmlsafe = false) {
 	return $result;
 }
 
-$input = utf8_encode("gregwegwergfdgfgsdasfæøåæøåfgewfwefgwegwgdsfg");
-echo "Input:<br>$input<br><br>";
-$result = compress("gregwegwergfdgfgsdasfæøåæøåfgewfwefgwegwgdsfg");
+$input = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lobortis, libero vestibulum tincidunt posuere, felis nisi lacinia libero, eu consectetur est libero eu diam. Integer urna urna, venenatis ac lobortis vel, pharetra a neque. Curabitur posuere diam sed tortor aliquam suscipit. Curabitur sodales, tortor vitae consectetur scelerisque, leo nisi laoreet ipsum, sed pellentesque enim dui ac leo. Nam sed porta leo. Nulla at facilisis lacus. Aliquam erat volutpat. Sed nec euismod dolor. Integer dictum magna tempus est blandit aliquet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin at massa nibh, et facilisis sapien. Etiam a justo at urna mattis faucibus a commodo diam. Curabitur sagittis convallis ante, vel laoreet ipsum cursus sed. Nulla eu lectus sem. Pellentesque viverra vulputate tortor, ut semper est viverra in. Mauris est tortor, fringilla a adipiscing non, consequat non eros.
+
+Proin posuere massa nec nisi ornare ut consectetur sem tempor. Integer volutpat pharetra felis eu lobortis. Proin lacinia tortor a augue vulputate ullamcorper. Phasellus laoreet mollis tempus. Aliquam tincidunt, elit cursus placerat ornare, nibh sem pharetra ipsum, ut tempus lacus quam id est. Phasellus in sem mauris. Aenean hendrerit bibendum dui, id aliquet dui eleifend vitae. Vivamus pellentesque mauris vel erat fringilla molestie. Vestibulum sodales pretium dolor nec commodo. Nunc mi leo, scelerisque at fermentum cursus, egestas non mi. Donec porttitor tempor velit eget vulputate. Integer tempor condimentum diam. Mauris nisi purus, varius nec porttitor sit amet, luctus condimentum sapien. Cras libero lectus, mattis at condimentum sit amet, ultrices et orci.
+
+Aliquam nisi nulla, tristique in facilisis id, venenatis non ligula. Vivamus pellentesque libero sit amet tellus tristique volutpat. Etiam commodo lacus nec velit blandit in vestibulum libero fringilla. Nullam eget sapien lacus, non tempor nulla. Nulla ac ligula ante. Maecenas placerat quam velit. Phasellus non urna arcu, dignissim hendrerit lorem. Praesent quis leo non sapien accumsan fringilla. Sed sed urna vitae elit gravida sagittis. Maecenas vestibulum nisl in sem porta vitae fermentum ante commodo.
+
+Nulla facilisi. Sed eget ligula vitae magna laoreet egestas a nec libero. Nunc tincidunt ligula nec nisi scelerisque euismod. Vivamus congue, magna a semper iaculis, neque eros mollis sem, eget semper urna purus sed odio. Etiam fringilla molestie nunc sit amet tincidunt. Suspendisse potenti. Donec nibh sem, pellentesque lobortis euismod non, scelerisque gravida enim. Fusce eu urna convallis mauris mattis hendrerit. Donec porta metus eu magna aliquam lobortis. Suspendisse hendrerit, magna sed porttitor varius, tellus leo feugiat justo, sed interdum sapien dui sit amet tellus. Aliquam magna enim, tincidunt ut tincidunt id, tempor non ipsum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer pharetra, magna eget mollis posuere, tortor nibh eleifend arcu, sed vehicula augue libero eget sapien. In hac habitasse platea dictumst. Nam et erat enim, non porta nulla. Praesent fermentum dolor hendrerit dui semper et dapibus justo fermentum.";
+
+echo "Input:<br>".utf8_encode($input)."<br><br>";
+$result = compress($input);
 echo "Compressed result:<br>$result<br><br>";
 $decom = utf8_encode(decompress($result));
 echo "Decompressed result:<br>$decom<br><br>";
-if ($input == $decom) {
+if (utf8_encode($input) == $decom) {
 	echo "Success!";
 } else {
 	echo "What... those aren't alike!";
